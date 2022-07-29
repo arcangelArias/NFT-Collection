098d25c606962d800106c1f85d76363af2d15e3d
angelArias <angel@angelarias.es>
2022-07-29 21:12:22 +0200

    primer commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xe4B9419ad4e6741d7fE1Db6729007e2c864F6135";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-swart-theta.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
